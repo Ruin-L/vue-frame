fd4dd1bf88f4faa429caa753ca588984c8598483
Ruin <1830275783@qq.com>
2022-07-26 16:59:24 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@
  * @Author: Ruin 🍭
  * @Date: 2022-01-25 16:22:24
  * @LastEditors: 刘引
- * @LastEditTime: 2022-03-11 14:55:52
+ * @LastEditTime: 2022-07-26 16:58:53
  */
 import { createApp } from "vue";
 
 import router from "./router/index"; //引入vue-router
 import store from "./store/index";
 import App from "./App.vue";
+import {createPinia} from 'pinia'
 // import
 import "normalize.css/normalize.css";
 const app = createApp(App);
+const pinia = createPinia()
 app.use(router); // 挂载到app上
 app.use(store); // 挂载到app上
+app.use(pinia)
 app.mount("#app");
