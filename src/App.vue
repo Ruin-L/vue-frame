15617f45b91030641d799620a33f358f59350525
刘引 <yin.liu@sunny-baer.com>
2025-03-19 14:07:07 +0800

    fix:去除app.vue多余注释

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<!--
- * @Description: {{ByRuin}}
- * @Version: 2.0
- * @Author: Ruin 🍭
- * @Date: 2022-01-25 16:22:24
- * @LastEditors: 刘引 liu.yin.work@foxmail.com
- * @LastEditTime: 2023-08-01 15:25:13
--->
-
 <template>
   <router-view></router-view>
 </template>
